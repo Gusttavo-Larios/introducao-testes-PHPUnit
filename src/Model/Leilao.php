d5d01bc15dbdca01014ae81721c75e0a411d7860
dev <gustavolarios077@gmail.com>
2023-11-28 15:48:13 -0400

    TDD

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -17,6 +17,17 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if (!empty($this->lances) && $this->ehDoUltimoLance($lance)) {
+            return;
+        }
+
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
+
         $this->lances[] = $lance;
     }
 
@@ -26,5 +37,28 @@ class Leilao
     public function getLances(): array
     {
         return $this->lances;
+    }
+
+    private function ehDoUltimoLance(Lance $lance)
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+
+                return $totalAcumulado;
+            },
+            0
+        );
+
+        return $totalLancesUsuario;
     }
 }
